4fd1a0069191e515f65850466d6bd317759098fb
Luba Pecheneva <lp5786766@gmail.com>
2022-06-23 15:58:12 -0400

    Updates

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+$(document).ready( () => {
+  $('.sidenav').sidenav();
+
+  //   image enlargement
+  $('.materialboxed').materialbox();
+
+  $('.carousel').carousel();
+
+//   const a = $('h1');
+//   a.click(() => {
+//     alert("a clicked!");
+//   })
+});
+
+
